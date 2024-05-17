e53d2b6a3aa69b7f2bfd1fdf2abc1657d1cc679c
Ralxm <rafael61111@gmail.com>
2024-05-17 15:50:33 +0100

    paths

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MainPage from './page/MainPage'
+import PaginaPrincipal from './page/MainPage'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
