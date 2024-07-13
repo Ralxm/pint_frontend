2a6ead931519df75f5b686fd23332e82b3be0c84
Ralxm <rafael61111@gmail.com>
2024-07-13 17:28:00 +0100

    ai2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './Universal/MainPage'
 import reportWebVitals from './reportWebVitals';
